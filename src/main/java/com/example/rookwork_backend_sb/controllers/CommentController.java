57329a2a2703618acebe64d261c59d6169bf6946
Tanas2k4 <bongbo355@gmail.com>
2026-03-13 14:44:48 +0700

    feat: tick feats

---
+++ src/main/java/com/example/rookwork_backend_sb/controllers/CommentController.java
@@ -0,0 +1,16 @@
+package com.example.rookwork_backend_sb.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/comments")
+public class CommentController {
+    /// Create comment
+
+    /// Update comment
+
+    /// Delete comment
+
+    /// Get comment by post id
+}
